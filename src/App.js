e8d0242de8a48058e1f8486f026880449d824a63
Yash Kumar <kumar.yash9313@gmail.com>
2021-09-17 23:59:28 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Button, makeStyles } from "@material-ui/core";
+import "./Assets/Sass/App.scss";
+import Image from "./Assets/Images/hello.png";
+const useStyles = makeStyles((themes) => ({
+  app: {
+    backgroundColor: themes.palette.grey[900],
+    height: "100vh",
+  },
+  button: {
+    backgroundColor: themes.palette.primary.main,
+    padding: themes.spacing(3),
+    margin: "auto",
+  },
+  divb: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  divi: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  head: {
+    textAlign: "center",
+    color: "white",
+    padding: themes.spacing(3),
+  },
+}));
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div container className={classes.app}>
+        <div className={classes.divi}>
+          <img src={Image} alt="logo"></img>
+        </div>
+        <div className={classes.divb}>
+          <Button
+            variant="contained"
+            className={classes.button}
+            href="https://discord.gg/cP6eFjvSHU"
+            target="_blank"
+          >
+            Join Discord Community Server
+          </Button>
+        </div>
+        <h3 className={classes.head}>Site Under Contruction</h3>
+      </div>
+    </>
   );
 }
 
